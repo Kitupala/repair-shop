c8109820cdbad2956cf4ea10fc9897a041edf8b3
Kitupala <kimmo.tuupanen@gmail.com>
2024-12-10 20:24:02 +0200

    add search

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,9 +1,28 @@
+import CustomerSearch from "@/app/(rs)/customers/CustomerSearch";
+import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
+import { db } from "@/db";
+
 export const metadata = {
-  title: "Customers",
+  title: "Customer Search",
 };
 
-function Customers() {
-  return <h2>Customers Page</h2>;
+type Props = {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+};
+
+async function Customers({ searchParams }: Props) {
+  const { searchText } = await searchParams;
+
+  if (!searchText) return <CustomerSearch />;
+
+  const results = await getCustomerSearchResults(searchText);
+
+  return (
+    <>
+      <CustomerSearch />
+      <p>{JSON.stringify(results)}</p>
+    </>
+  );
 }
 
 export default Customers;
--- src/app/(rs)/tickets/page.tsx
+++ src/app/(rs)/tickets/page.tsx
@@ -1,9 +1,37 @@
+import TicketSearch from "@/app/(rs)/tickets/TicketSearch";
+import { getOpenTickets } from "@/lib/queries/getOpenTickets";
+import { getTicketSearchResults } from "@/lib/queries/getTicketSearchResults";
+
 export const metadata = {
-  title: "Tickets",
+  title: "Ticket Search",
 };
 
-function Tickets() {
-  return <h2>Tickets Page</h2>;
+type Props = {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+};
+
+async function Tickets({ searchParams }: Props) {
+  const { searchText } = await searchParams;
+
+  if (!searchText) {
+    const results = await getOpenTickets();
+
+    return (
+      <>
+        <TicketSearch />
+        <p>{JSON.stringify(results)}</p>
+      </>
+    );
+  }
+
+  const results = await getTicketSearchResults(searchText);
+
+  return (
+    <>
+      <TicketSearch />
+      <p>{JSON.stringify(results)}</p>
+    </>
+  );
 }
 
 export default Tickets;
