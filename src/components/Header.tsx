b59252eb4e6ab5c2d640ed29835dda4051539ae2
Kitupala <kimmo.tuupanen@gmail.com>
2024-12-10 20:25:38 +0200

    add NavButtonMenu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,11 @@ import Link from "next/link";
 import { ModeToggle } from "@/components/ModeToggle";
 import NavButton from "@/components/NavButton";
 import { Button } from "@/components/ui/button";
+import NavButtonMenu from "@/components/NavButtonMenu";
 
 function Header() {
   return (
-    <header className="animate-slide sticky top-0 z-20 h-12 border-b bg-background p-2">
+    <header className="sticky top-0 z-20 h-12 animate-slide border-b bg-background p-2">
       <div className="flex h-8 w-full items-center justify-between">
         <div className="flex items-center gap-2">
           <NavButton href="/home" label="Home" icon={HomeIcon} />
@@ -24,7 +25,14 @@ function Header() {
         </div>
         <div className="flex items-center">
           <NavButton href="/tickets" label="Tickets" icon={File} />
-          <NavButton href="/customers" label="Customers" icon={UsersRound} />
+          <NavButtonMenu
+            icon={UsersRound}
+            label="Customers Menu"
+            choices={[
+              { title: "Search Customers", href: "/customers" },
+              { title: "New Customer", href: "/customers/form" },
+            ]}
+          />
           <ModeToggle />
           <Button
             variant="ghost"
