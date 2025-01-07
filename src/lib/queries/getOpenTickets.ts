e9dece2b3ca0931141a3509a53d8cdf942c5ab64
Kitupala <kimmo.tuupanen@gmail.com>
2025-01-07 18:02:57 +0200

    modify query

--- src/lib/queries/getOpenTickets.ts
+++ src/lib/queries/getOpenTickets.ts
@@ -1,20 +1,21 @@
 import { db } from "@/db";
 import { tickets, customers } from "@/db/schema";
-import { eq } from "drizzle-orm";
+import { asc, eq } from "drizzle-orm";
 
 export async function getOpenTickets() {
-  const results = await db
+  return db
     .select({
+      id: tickets.id,
       ticketDate: tickets.createdAt,
       title: tickets.title,
       firstName: customers.firstName,
       lastName: customers.lastName,
       email: customers.email,
       tech: tickets.tech,
+      completed: tickets.completed,
     })
     .from(tickets)
     .leftJoin(customers, eq(tickets.customerId, customers.id))
-    .where(eq(tickets.completed, false));
-
-  return results;
+    .where(eq(tickets.completed, false))
+    .orderBy(asc(tickets.createdAt));
 }
