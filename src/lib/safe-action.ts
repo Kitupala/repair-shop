4d09f48d609dc09c78f806687f598f32d64b4a68
Kitupala <kimmo.tuupanen@gmail.com>
2025-01-09 10:06:07 +0200

    modify error handling

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,6 +1,7 @@
 import { createSafeActionClient } from "next-safe-action";
 import { z } from "zod";
 import * as Sentry from "@sentry/nextjs";
+import type { NeonDbError } from "@neondatabase/serverless";
 
 export const actionClient = createSafeActionClient({
   defineMetadataSchema() {
@@ -10,6 +11,14 @@ export const actionClient = createSafeActionClient({
   },
   handleServerError(e, utils) {
     const { clientInput, metadata } = utils;
+
+    if (e.constructor.name === "NeonDbError") {
+      const { code, detail } = e as NeonDbError;
+      if (code === "23505") {
+        return `Unique entry required. ${detail}`;
+      }
+    }
+
     Sentry.captureException(e, (scope) => {
       scope.clear();
       scope.setContext("serverError", { message: e.message });
@@ -19,7 +28,7 @@ export const actionClient = createSafeActionClient({
       return scope;
     });
 
-    if (e.constructor.name === "DatabaseError") {
+    if (e.constructor.name === "NeonDbError") {
       return "Database Error: Your data did not save. Support will be notified.";
     }
 
