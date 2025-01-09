6a860835bf7559307eef6c82272d0aa6ddee085e
Kitupala <kimmo.tuupanen@gmail.com>
2025-01-09 10:09:36 +0200

    refactor code

--- src/app/(rs)/tickets/form/page.tsx
+++ src/app/(rs)/tickets/form/page.tsx
@@ -74,13 +74,14 @@ export default async function TicketFormPage({ searchParams }: Props) {
       if (isManager) {
         kindeInit(); // Initializes the Kinde Management API
         const { users } = await Users.getUsers();
-        const techs = users
-          ? users.map((user) => ({
-              id: user.email!,
-              description: user.email!,
-            }))
-          : [];
-        return <TicketForm customer={customer} techs={techs} />;
+        const techs =
+          users?.map((user) => ({
+            id: user.email?.toLowerCase() || "",
+            description: user.email?.toLowerCase() || "",
+          })) ?? [];
+        return (
+          <TicketForm customer={customer} techs={techs} isManager={isManager} />
+        );
       } else {
         return <TicketForm customer={customer} />;
       }
@@ -110,7 +111,14 @@ export default async function TicketFormPage({ searchParams }: Props) {
               description: user.email!,
             }))
           : [];
-        return <TicketForm customer={customer} ticket={ticket} techs={techs} />;
+        return (
+          <TicketForm
+            customer={customer}
+            ticket={ticket}
+            techs={techs}
+            isManager={isManager}
+          />
+        );
       } else {
         const isEditable =
           user.email?.toLowerCase() === ticket.tech.toLowerCase();
